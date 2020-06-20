b037db666cfa4800d67100262f6ccb452abae040
guanghuiwd <42063491+guanghuiwd@users.noreply.github.com>
2020-06-20 17:30:20 +0800

    Update lcof15.java

--- lcof15.java
+++ lcof15.java
@@ -1,8 +1,17 @@
-public int hammingWeight(int n) {
+public int hammingWeight2(int n) {
     int result = 0;
     while(n != 0) {
         result += n % 2;  // 负数要避免这种操作
         n = n / 2;
     }
     return result;
+}
+
+public int hammingWeight(int n) {
+    int res=0;
+    while (n != 0) {
+        res += n & 1;
+        n = n >>> 1;
+    }
+    return res;
 }
